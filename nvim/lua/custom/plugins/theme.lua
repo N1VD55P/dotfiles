d9b5ef2e97b2dee1aec668168923003766f622e2
N1VD55P <n1vdeepnain@gmail.com>
2026-03-27 05:32:50 +0530

    i dont know what i chnaged

--- nvim/lua/custom/plugins/theme.lua
+++ nvim/lua/custom/plugins/theme.lua
@@ -1,16 +1,29 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		name = "nightfox",
-		priority = 1000,
-		config = function()
-			require("nightfox").setup({
-				options = {
-					transparent = false,
-				},
-				flavour = "duskfox",
-			})
-			vim.cmd.colorscheme("duskfox")
-		end,
-	},
+        {
+                "folke/tokyonight.nvim",
+                priority = 1000,
+                config = function()
+                        require("tokyonight").setup({
+                                style = "storm",
+                                on_colors = function(colors)
+                                        colors.bg = "#0a0e27"
+                                        colors.bg_dark = "#050810"
+                                        colors.bg_float = "#0f1219"
+                                end,
+                                on_highlights = function(hl, c)
+                                        hl.Function = { fg = c.blue, bold = true }
+                                        hl.Keyword = { fg = c.magenta, bold = true }
+                                        hl.Statement = { fg = c.purple, bold = true }
+                                        hl.String = { fg = c.green }
+                                        hl.Number = { fg = c.orange }
+                                        hl.Boolean = { fg = c.orange, bold = true }
+                                        hl.Constant = { fg = c.cyan, bold = true }
+                                        hl.Type = { fg = c.blue, bold = true }
+                                        hl.Comment = { fg = c.dark5, italic = true }
+                                        hl.Variable = { fg = c.fg }
+                                end,
+                        })
+                        vim.cmd.colorscheme("tokyonight-storm")
+                end,
+        },
 }
--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -2,35 +2,38 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    priority = 100,
+    build = function()
+      require("nvim-treesitter.install").prefer_git = false
+      vim.cmd(":TSUpdate")
+    end,
+    event = { "BufReadPre", "BufNewFile" },
+    cmd = { "TSInstall", "TSUpdate", "TSUninstall" },
     opts = {
-        ensure_installed = {
-          "lua", "python", "javascript", "typescript", "vimdoc", "vim",
-          "regex", "sql", "dockerfile", "toml", "json",
-          "java", "go", "graphql", "yaml",
-          "make", "cmake", "markdown", "markdown_inline", "bash",
-          "tsx", "css", "html", "c", "cpp",
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "gnn",
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
+      ensure_installed = {
+        "lua", "python", "javascript", "typescript", "vimdoc", "vim",
+        "regex", "sql", "dockerfile", "toml", "json",
+        "java", "go", "graphql", "yaml",
+        "make", "cmake", "markdown", "markdown_inline", "bash",
+        "tsx", "css", "html", "c", "cpp",
+      },
+      sync_install = true,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+      },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
         },
       },
+    },
   },
 }
